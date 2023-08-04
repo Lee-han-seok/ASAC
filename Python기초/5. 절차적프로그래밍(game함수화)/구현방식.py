0fed2b07e96f39900f72661be2f742f9ce88fec6
이한석 / Hanseok Lee <onestone0311@gmail.com>
2023-08-04 16:34:03 +0900

    Update 구현방식.py

--- Python기초/5. 절차적프로그래밍(game함수화)/구현방식.py
+++ Python기초/5. 절차적프로그래밍(game함수화)/구현방식.py
@@ -1,1 +1,10 @@
 # 구현 및 동작 방식
+# game_ex.py : 엔트리 파일로 가장 단순화된 구성으로 실제 실행되는 파일
+# util -> _init_.py : 사용되는 print 함수 정의(실질적인 의미는 없으나 실습을 위함!)
+## log() :  게임 구동과 무관한 print 함수
+## game_print() : 게임 구동과 상관있는 print 함수
+# engine -> ai.py : 난수 추출하는 함수 정리
+# engine -> play.py : step별 상세 절차 정리
+# config -> -init_ : 고정되어 있는 값들 총 정리 (출력 문자 등)
+
+ 
